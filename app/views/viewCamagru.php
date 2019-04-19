5fe994a0cb146a4b65ec5489c609560bc0808db4
Clement FOLTRAN <clfoltra@e2r7p4.42.fr>
2019-04-19 15:51:57 +0200

    restyle the camagru page

--- app/views/viewCamagru.php
+++ app/views/viewCamagru.php
@@ -1,27 +1,29 @@
-
-<div class="center">
-    <video id="video" width="640px" autoplay></video>
-    <canvas class="center" id="canvas" value="pic" height="480px" width="640px"></canvas>
-    <div class="info" id="photoInfo"></div>
-    <div id="cam-button">
-        <button id="snap" type="button" class="btn-round rounded"><i class="fas fa-camera"></i></button>
-        <button id="snap-push" type="submit" name="pic" class="btn-round rounded"><i class="fas fa-check"></i></button>
-    </div>
+<div id="camagru">
+    <?php session_start() ?>
+    <h1>Hello <?= $_SESSION['login'] ?></h1>
+    <section id="cam">
+        <video id="video" width="640px" autoplay></video>
+        <canvas id="canvas" value="pic" height="480px" width="640px"></canvas>
+        <div class="info" id="photoInfo"></div>
+        <div id="cam-button">
+            <button id="snap" type="button" class="btn-round rounded"><i class="fas fa-camera"></i></button>
+            <button id="snap-push" type="submit" name="pic" class="btn-round rounded"><i class="fas fa-check"></i></button>
+        </div>
+    </section>
+    <section id="cards" class="cards">
+        <?php
+        $index = -1;
+        session_start();
+        if ($photos != null): 
+            foreach ($photos as $photo): $index++?>
+                <article>
+                    <img class="article-img" src="data:image/jpeg;base64,<?= $photo->getPhoto()?>">
+                    <div class="article-title">
+                        <i class="fas fa-trash-alt" onclick="dropPhoto(<?= $photos[$index]->getId()?>, <?= $index ?>)"></i>
+                    </div>
+                </article>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </section>
 </div>
-<h1 style="margin-top: 40px">Ma librairie</h1>
-<section class="cards">
-    <?php
-    $index = -1;
-    session_start();
-    if ($photos != null): 
-        foreach ($photos as $photo): $index++?>
-            <article>
-                <img class="article-img" src="data:image/jpeg;base64,<?= $photo->getPhoto()?>">
-                <div class="article-title">
-                    <i class="fas fa-trash-alt" onclick="dropPhoto(<?= $photos[$index]->getId()?>, <?= $index ?>)"></i>
-                </div>
-            </article>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</section>
 <script src="<?= URL ?>scripts/camagru.js"></script>
