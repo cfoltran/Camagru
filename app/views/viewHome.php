ab7f96b34edfe486101967c801c80b7bf3e01cfb
Clement FOLTRAN <clfoltra@e2r7p4.42.fr>
2019-04-19 13:43:13 +0200

    change the comportement on like

--- app/views/viewHome.php
+++ app/views/viewHome.php
@@ -12,7 +12,11 @@
                 <article>
                     <img class="article-img"  onclick="displayModalPic(<?= $index ?>)" src="data:image/jpeg;base64,<?= $photo->getPhoto()?>">
                     <div class="article-title">
-                        <i onclick="like(<?= $photo->getId() ?>, <?=$index?>, <?= $loggued ?>)"class="fas fa-thumbs-up"> <?= $photo->getLikeNumber($photo->getId()) ?></i>
+                        <?php if ($loggued == true): ?>
+                            <i onclick="like(<?= $photo->getId() ?>, <?=$index?>, <?= $loggued ?>)"class="fas fa-thumbs-up"> <?= $photo->getLikeNumber($photo->getId()) ?></i>
+                        <?php else: ?>
+                            <a style="color: black" href="<?URL?>?url=login"><i class="fas fa-thumbs-up"> <?= $photo->getLikeNumber($photo->getId()) ?></i></a>
+                        <?php endif; ?>
                         <i class="fas fa-comments"> <?= $photo->getCommentNumber()?></i>
                     </div>
                 </article>
@@ -24,8 +28,12 @@
     <div class="modal-content">
         <img id="modal-img">
         <h2>Commentaires</h2>
-        <input class="input-com comment" id="comment" type="text" placeholder="Leave a comment">
-        <button class="btn-blue" style="float: right" onclick="comment(<?= $photos[0]->getId() ?>)">Comment</button>     
+        <?php if ($loggued == true): ?>
+            <input class="input-com comment" id="comment" type="text" placeholder="Leave a comment">
+            <button class="btn-blue" style="float: right" onclick="comment(<?= $photos[0]->getId() ?>)">Comment</button>
+        <?php else : ?>
+            <a href="<?= URL ?>?url=login">Log in</a> or <a href="<? URL ?>?url=register">register</a> to comment a publication
+        <?php endif; ?>
     </div>
 </div>
 <script src="<?= URL ?>scripts/home.js"></script>
