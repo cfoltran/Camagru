68bb3560ca33a750fa4b330d895fcd228bc3809c
clementfoltran <clfoltra@student.42.fr>
2019-04-22 18:37:25 +0200

    change css of the publication page

--- app/views/viewPublication.php
+++ app/views/viewPublication.php
@@ -1,14 +1,14 @@
 <div id="pub">
-    <div id="pub-info">
+    <section id="pub-info">
         <i><?= $pub->getDate() ?></i>
         <h2>Publication of <?= $pub->getLoginOfPhotoOwner() ?></h2>
-        <img src="data:image/jpeg;base64,<?= $pub->getPhoto()?>">
-    </div>
+        <img width="640px" src="data:image/jpeg;base64,<?= $pub->getPhoto()?>">
+    </section>
     <?php
         session_start();
         $login = $_SESSION['login'];
         if ($login != null): ?>
-        <div id="com-zone">
+        <section id="com-zone">
             <h2>Comments</h2>
             <input class="input-com comment" id="comment" type="text" placeholder="Leave a comment">
             <button class="btn-blue" onclick="comment(<?= $pub->getId() ?>, '<?= $login ?>', <?= $pub->getIdUser() ?>)">Comment</button>
@@ -19,7 +19,7 @@
                         <p><b class="btn-blue"><?= $pub->getLoginOfComment($comment['id_user'])?></b> <?= $comment['comment'] ?></p>
                     </div>
             <?php endforeach; ?>
-        </div>
+        </section>
     <?php else : ?>
         <a href="<?= URL ?>?url=login">Log in</a> or <a href="<? URL ?>?url=register">register</a> to comment a publication
     <?php endif; ?>
