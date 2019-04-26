46de5d90dd928d6aed145c2bde2af44af03d10f8
clementfoltran <clfoltra@student.42.fr>
2019-04-26 11:49:26 +0200

    update config

--- config/setup.php
+++ config/setup.php
@@ -4,14 +4,6 @@
     $co = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     if (!$co)
         die("An error occured 😥\n");
-    $query = "CREATE DATABASE camagrudb";
-    $req = $co->prepare($query);
-    $req->execute();
-    if ($req)
-        echo "Database created 👌 \n";
-    else
-        echo "An error occured 😥\n";
-    $req->closeCursor();
     $all_query = file_get_contents('./camagrudb.sql');
     try {
         $co->exec($all_query);
